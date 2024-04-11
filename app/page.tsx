f434b63c7181dda1b901b67ec9889c45bccfcecf
BeagleXV <beaglexv@gmail.com>
2024-04-11 15:31:36 -0300

    cores lil

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-rosa">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col lg:flex-row">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto">
+          
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 fonte-maior"
+            href="https://nouns.build/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -19,22 +18,29 @@ export default function Home() {
             <Image
               src="/gnar.png"
               alt="Vercel Logo"
-              width={100}
-              height={24}
+              width={30}
+              height={34}
               priority
             />
           </a>
         </div>
       </div>
 
-      <div>
+      <div className="lg:w-1/4 mx-auto pl-8">
         <Image
           src="/lilgnar.png"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={380}
+          height={250}
           priority
         />
+        <div className="mx-auto mb-4">
+          <p>
+          
+          
+          Our Social Project aims to bring together young skateboarders to explore skate spots across the city while prioritizing safety, skill development, and fun. With the guidance of experienced skaters, we seek to create a supportive environment where children can overcome fears, learn new skills, and build confidence through skateboarding challenges.
+          </p>
+        </div>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
